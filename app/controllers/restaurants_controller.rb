7075080ef99ad52c065712d9aa75a0f534e02539
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-13 15:51:53 -0400

    fixed routes to resource's' and finished index view page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,10 +2,14 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
+
+    render :index
   end
 
   def show
     @restaurant = Restaurant.find_by(params[:id])
+
+    render :show
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :restaurants do
-    resource :reservations
+  resources :restaurants do
+    resources :reservations
   end
-  root 'restaurant#index'
+  root 'restaurants#index'
 end
