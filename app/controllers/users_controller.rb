dad62a845b85ccefe8c5476bdbd34447d0492864
sbcreates <drawsarahb@gmail.com>
2017-09-14 17:03:02 -0400

    Added sessions controller with new, destroy, and create methods/views. Added log in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,9 @@ def create
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
 
-
-
    if @user.save
-     # if the picture gets saved, generate a get request to "/pictures" (the index)
      redirect_to root_url
    else
-     # otherwise render new.html.erb
      render :new
    end
 end
