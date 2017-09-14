2a84be3e6d2626f80927b60c67714651c02a23e7
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-14 16:04:37 -0400

    added form for All avaliable seats and added show page for your reservation and a method for custom validation for restaurant capacity

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,13 +1,14 @@
 class Reservation < ApplicationRecord
   validates :party_size, :reservation_time, presence: true
-
+  validate :can_be_made
   belongs_to :user
   belongs_to :restaurant
 
   # validation method
-  def available_times(datetime)
-
-    # self.restaurant.reservation_time.sum(datetime)
+  def can_be_made
+    if party_size  > restaurant.capacity
+      errors.add(:base, "Party size is too big, reservation can't be made!")
+    end
   end
 
 
