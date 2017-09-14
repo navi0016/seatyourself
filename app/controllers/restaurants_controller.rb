98aa86aabb7433328b6c1bc94a0fd5fbba18d192
sbcreates <drawsarahb@gmail.com>
2017-09-14 07:43:12 -0400

    test

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,14 +2,11 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
-
-
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     @reservation = @restaurant.reservations.new
-
   end
 
   def new
