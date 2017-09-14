d97fa301d9d94f93260978377f5b7db5558d62e9
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-13 20:30:38 -0400

    added Reservation form to restaurant show/view

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,8 +4,4 @@ class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
 
-  # def date_select
-  #   datetime_select (@reservation, "reservation_time", default: 1.hour.from_now, ampm: true, start_hour: @restaurant.start_time.hour.to_i, end_hour: @restaurant.end_time.hour.to_i,datetime_separator: 'Time:',discard_minute: true,start_year: Date.today.year)
-  # end
-
 end
