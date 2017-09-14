684368aebb8cd5bff5b8ed7972994908625b521c
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-14 16:18:18 -0400

    added create and new methods to user_controller and added form for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+def new
+  @user= User.new
+end
+
+def create
+  @user = User.new
+
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+
+
+   if @user.save
+     # if the picture gets saved, generate a get request to "/pictures" (the index)
+     redirect_to root_url
+   else
+     # otherwise render new.html.erb
+     render :new
+   end
+end
+
+
 end
