7a4eb40d31e49b7e4508c73487e274e6a7061b16
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-15 15:05:56 -0400

    bug fixedgit add .git add .!

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,19 +3,19 @@ class ReservationsController < ApplicationController
   def index
     # TODO: Find out why some bookings fail to save (eg. transaction is rolled back).
     # TODO: Ensure that user is directed away from index view if essential params are missing.
-
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @reservation = @restaurant.reservations.new
-    @selected_time=DateTime.new(params[:reservation]["reservation_time(1i)"].to_i,params[:reservation]["reservation_time(2i)"].to_i,params[:reservation]["reservation_time(3i)"].to_i, params[:reservation]["reservation_time(4i)"].to_i)
-
     @year = params[:reservation]["reservation_time(1i)"].to_i
     @month = params[:reservation]["reservation_time(2i)"].to_i
     @day = params[:reservation]["reservation_time(3i)"].to_i
 
-    if @year && @month && @day
-    else
-      redirect_to 'http://google.com'
+    if @year ==0 || @month==0 || @day==0
+      redirect_to root_path
     end
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.new
+    @selected_time=DateTime.new(params[:reservation]["reservation_time(1i)"].to_i,params[:reservation]["reservation_time(2i)"].to_i,params[:reservation]["reservation_time(3i)"].to_i, params[:reservation]["reservation_time(4i)"].to_i)
+
+
+
   end
 
 
@@ -32,7 +32,7 @@ class ReservationsController < ApplicationController
       redirect_to root_path
     else
       # debugger
-      debugger
+      # debugger
       @reservation.save!
       flash[:notice] = "You're reservation has been book!"
       redirect_to restaurant_reservation_path @restaurant,@reservation
