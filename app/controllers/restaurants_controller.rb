4f81a4c4add19eb06faf93d4994d5e8b01a16d37
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-14 20:22:30 -0400

    Addded ensure_user_logged_in method, and added user_owner new,edit,create,update,destrory methods and new restaurant form for owner_user - Gave authorization to different users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,8 @@
 class RestaurantsController < ApplicationController
+   before_action :ensure_logged_in, except: [:show, :index]
+   before_action :load_restaurant, only: [:create, :edit, :update, :destroy]
+   before_action :ensure_user_owns_restaurant, only: [:create, :edit, :update, :destroy]
+
 
   def index
     @restaurants = Restaurant.all
@@ -13,6 +17,20 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.email = params[:restaurant][:email]
+    @reservation.location = params[:restaurant][:location]
+    @restaurant.start_time = params[:restaurant][:start_time]
+    @restaurant.end_time = params[:restaurant][:end_time]
+    @restaurant.capacity = params[:restaurant][:capacity]
+    @restaurant.use_id = current_user[:id]
+
+    if @restaurant.save
+    redirect_to root_path
+  else
+    render :new
+  end
+
   end
 
   def update
@@ -31,9 +49,19 @@ class RestaurantsController < ApplicationController
   end
 
   def destroy
-  @restaurant = Restaurant.find(params[:id])
   @restaurant.destroy
   redirect_to root_path
+  end
+
+  def load_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def ensure_user_owns_restaurant
+  unless current_user == @restaurant.user
+    flash[:alert] = "You are not authorized to view this page!"
+    redirect_to new_session_url
+  end
   end
 
 end
